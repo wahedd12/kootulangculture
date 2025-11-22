0a7cc1849acb9eab034cb3e03956acac0872f175
Waheed Akinrele <waheedakinrele@yahoo.com>
2025-11-22 18:51:35 +0100

    Use Paystack public key for frontend subscription

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -30,7 +30,7 @@ const Subscribe = () => {
   const paystackProps = {
     email: currentUser?.email,
     amount: 5000 * 100, // 5000 NGN in kobo
-    publicKey: "sk_test_c8ca1c3994df29f667685b63c46fc128f9e12a8d",
+    publicKey: "pk_test_77dd0b5408bb0b896b387ce76065a97c82eb7498",
     currency: "NGN",
     text: "Upgrade to Premium (₦5,000 / 1 month)",
     onSuccess: () => handleUpgradeBackend(),
